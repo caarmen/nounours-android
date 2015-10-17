26b30c981145bf0e963f7ab9d6874f6383558106
Carmen Alvarez <c@rmen.ca>
2015-10-18 00:10:58 +0200

    Fix NPE on configuration change

--- app/src/main/java/ca/rmen/nounours/MainActivity.java
+++ app/src/main/java/ca/rmen/nounours/MainActivity.java
@@ -229,7 +229,7 @@ public class MainActivity extends Activity {
         if (animationMenu != null) {
             animationMenu.setEnabled(!nounoursIsBusy);
             animationMenu.setVisible(theme != null && !theme.getAnimations().isEmpty());
-            setupAnimationMenu(animationMenu.getSubMenu());
+            if (!nounoursIsBusy) setupAnimationMenu(animationMenu.getSubMenu());
         }
         MenuItem recordingMenu = menu.findItem(R.id.menu_start_recording);
         if (recordingMenu != null) {
